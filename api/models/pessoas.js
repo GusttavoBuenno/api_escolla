b34d934af3f426e0219d24cb233aa462bcf33f07
Gustavo <gustavobueno05@outlook.com.br>
2022-06-27 17:30:18 -0300

    up2

--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -1,19 +1,43 @@
-'use strict'
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const Pessoas = sequelize.define('Pessoas', {
-    nome: DataTypes.STRING,
-    ativo: DataTypes.BOOLEAN,
-    email: DataTypes.STRING,
-    role: DataTypes.STRING
-  }, { paranoid: true })     // usando paranoid, para que nenhum registo seja realmente deletado ficando no banco 
-  Pessoas.associate = function(models) {
-    Pessoas.hasMany(models.Turmas, {
-      foreignKey: 'docente_id'
-    }) 
-    Pessoas.hasMany(models.Matriculas, {
-      foreignKey: 'estudante_id'
-    })
+  const Pessoas = sequelize.define(
+    "Pessoas",
+    {
+      nome: {
+        type:DataTypes.STRING,
+      validate: {
+        funcaoValidadora: function(dado) {
+          if (dado.length < 3 ) throw new Error('o Campo nome deve ter mais de 3 caracteres.')
+        }
+      }},
+      ativo: DataTypes.BOOLEAN,
+      email: {
+        type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          args: true,
+          msg: 'dado do tipo e-mail inválido'               //valindao email
+        }
+      }},
+      role: DataTypes.STRING,
+    },
+    { paranoid: true,
+      defaultScope: {
+        where: { ativo: true}
+      },
+      scopes: {
+        todos: { where: {} },
 
-  }
-  return Pessoas
-}
+      }
+     }
+  ); // usando paranoid, para que nenhum registo seja realmente deletado ficando no banco
+  Pessoas.associate = function (models) {
+    Pessoas.hasMany(models.Turmas, {
+      foreignKey: "docente_id",
+    });
+    Pessoas.hasMany(models.Matriculas, {
+      foreignKey: "estudante_id",
+    })
+  };
+  return Pessoas;
+};
--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -4,17 +4,18 @@ const PessoaController = require('../controllers/PessoaController')
 const router = Router()
 
 router
-  .get('/pessoas', PessoaController.pegaTodasAsPessoas)
+  .get('/pessoas', PessoaController.pegaPessoasAtivas)
+  .get('/pessoas/todos', PessoaController.pegaTodasAsPessoas)
   .get('/pessoas/:id', PessoaController.pegaUmaPessoa)
-  .post('/pessoas', PessoaController.criaPessoa)
-  .put('/pessoas/:id', PessoaController.atualizaPessoa)
-  .delete('/pessoas/:id', PessoaController.apagaPessoa)
   .get('/pessoas/:estudanteId/matricula/:matriculaId',  PessoaController.pegaUmaMatricula)
+  .post('/pessoas', PessoaController.criaPessoa)    
+  .post('/pessoas/:estudanteId/matricula/:matriculaId/restaura', PessoaController.restauraMatricula)
   .post('/pessoas/:estudanteId/matricula', PessoaController.criaMatricula)
   .post('/pessoas/:id/restaura', PessoaController.restauraPessoa)
   .put('/pessoas/:estudanteId/matricula/:matriculaId',  PessoaController.atualizaMatricula)
+  .put('/pessoas/:id', PessoaController.atualizaPessoa) 
   .delete('/pessoas/:estudanteId/matricula/:matriculaId',  PessoaController.apagaMatricula)
-  .post('/pessoas/:estudanteId/matricula/:matriculaId/restaura', PessoaController.restauraMatricula)
-
+  .delete('/pessoas/:id', PessoaController.apagaPessoa)
+  
 
 module.exports = router
